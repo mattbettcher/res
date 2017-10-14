d55e5c5cf873f57a7377b908a0c6e58614ec4ee2
Matthew Bettcher <mattbettcher@gmail.com>
2017-10-14 19:14:59 -0400

    Added minifb & rand & setup simple test window. Added launch and tasks json for vscode.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate minifb;
+extern crate rand;
+
+use minifb::{Key, WindowOptions, Window, Scale};
+
+const WIDTH: usize = 256;
+const HEIGHT: usize = 240;
+
 fn main() {
-    println!("Hello, world!");
+    // window test stuff
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let options = WindowOptions { resize: false, scale: Scale::X2, ..WindowOptions::default()};
+
+    let mut window = Window::new("Test - ESC to exit",
+                                 WIDTH,
+                                 HEIGHT,
+                                 options).unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = rand::random::<u32>(); // write something more funny here!
+        }
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window.update_with_buffer(&buffer).unwrap();
+    }
 }
